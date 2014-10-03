f235aed524a86c2f3516b7a8f26b2c6b7c5ba678
Louis Cadet <cadet.louis@gmail.com>
2014-10-03 10:09:11 +0200

    Git Testing

--- advchecker/urls.py
+++ advchecker/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
                        url(r'^$', 'advchecker.views.index', name='index'),
                        url(r'^campagnes/', include('campagnes.urls')),
