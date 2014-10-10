f3f07c520cd8dfab6c15c2c155e27ac6ada38fb5
Louis Cadet <cadet.louis@gmail.com>
2014-10-10 12:14:01 +0200

    Adding TODO to views.py

--- advchecker/views.py
+++ advchecker/views.py
@@ -8,7 +8,11 @@ def index(request):
     for e in campagnes_enseigne_list:
         e_magasins_list = Magasin.objects.filter(enseigne=e)
         campagnes_magasin_list[e] = e_magasins_list
+        print campagnes_magasin_list
 
     context = {'campagnes_enseigne_list': campagnes_enseigne_list,
                'campagnes_magasin_list': campagnes_magasin_list}
     return render(request, 'advchecker/index.html', context)
+
+
+## Class based view
