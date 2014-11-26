47cc043ba49256b014f56568e37767b79b5495c9
Louis Cadet <cadet.louis@gmail.com>
2014-11-26 11:31:56 +0100

    Add static root

--- advchecker/settings.py
+++ advchecker/settings.py
@@ -87,6 +87,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = '/srv/http/dev/adchecker/static/'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
