acba8dd82cf4b8fba8a2ea57e4478fa59e516d50
Louis Cadet <cadet.louis@gmail.com>
2014-11-26 10:24:44 +0100

    Correct import error for production

--- advchecker/urls.py
+++ advchecker/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from views import HomeView
+from advchecker.views import HomeView
 from testapp.views import TestView
 
 
